4fc3cdceb934f710b93a5598df271d544926d7e3
joão peres <joaopedropereszz0504@gmail.com>
2024-08-27 23:06:03 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import {Fragment} from "react";
+import HomeRegister from "@/src/home-register";
+
+export default function Home() {
+  return (
+    <Fragment>
+      <HomeRegister/>
+    </Fragment>
+  );
+}
---
+++ src/home-register/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./home-register.module.css"
+import {Fragment} from "react";
+
+export default function HomeRegister () {
+    return (
+        <Fragment>
+            <div className={styles.containerHome}>
+                Ola munfd dsandoasihdaslkjb  kjsahdka
+            </div>
+        </Fragment>
+    )
+}
