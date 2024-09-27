9106a809d71c8288a60cb9dedb25e9b7baf1f7c3
joão peres <joaopedropereszz0504@gmail.com>
2024-09-26 22:11:00 -0300

    Feat: finalizing the work section

--- src/front-end/molecules/projects-cards/index.tsx
+++ src/front-end/molecules/projects-cards/index.tsx
@@ -1,6 +1,7 @@
 import {Fragment} from "react";
 import styles from "./projects-cards.module.css"
 import Image from "next/image";
+import TextField from "../../atoms/text-field";
 
 type Props = {
     imageCard: any
@@ -13,6 +14,12 @@ export default function ProjectsCards ({imageCard, title, description} : Props) 
         <Fragment>
             <div className={styles.containerProjects}>
                 <Image src={imageCard} width={300}  height={300} alt={"image card"}/>
+                <div className={styles.containerTitle}>
+                    <TextField label={title}/>
+                </div>
+                <div className={styles.containerDescription}>
+                    <TextField label={description}/>
+                </div>
             </div>
         </Fragment>
     )
--- src/front-end/organisms/work-section/index.tsx
+++ src/front-end/organisms/work-section/index.tsx
@@ -5,31 +5,38 @@ import agriculture from "../../../../public/assets/agriculture.webp"
 import harvest  from "../../../../public/assets/harvest.webp"
 import environmentalEducation from "../../../../public/assets/environmental-education.webp"
 import composting from "../../../../public/assets/composting.webp"
+import TextField from "../../atoms/text-field";
 
 export default function WorkSection() {
     return (
         <Fragment>
             <div className={styles.containerWorkSection}>
+                <div className={styles.text}>
+                    <TextField label={"Ultimos trabalhos realizados"}/>
+                </div>
+                <div className={styles.title}>
+                    <TextField label={"Explore nossos projetos"}/>
+                </div>
                 <div className={styles.containerBox}>
                     <ProjectsCards
                         imageCard={agriculture}
-                        title={""}
-                        description={""}
+                        title={"Agricultura"}
+                        description={"Práticas sustentáveis para cultivo em ambientes urbanos."}
                     />
                     <ProjectsCards
                         imageCard={harvest}
-                        title={""}
-                        description={""}
+                        title={"Colheita"}
+                        description={"Melhores modos para colheita que garantem a frescura dos produtos."}
                     />
                     <ProjectsCards
                         imageCard={environmentalEducation}
-                        title={""}
-                        description={""}
+                        title={"Compostagem"}
+                        description={"Transformação de resíduos orgânicos em adubo natural."}
                     />
                     <ProjectsCards
                         imageCard={composting}
-                        title={""}
-                        description={""}
+                        title={"Educação Ambiental"}
+                        description={"Programas de conscientização sobre a importância da agricultura urbana."}
                     />
                 </div>
             </div>
