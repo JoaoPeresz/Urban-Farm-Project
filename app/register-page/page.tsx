b8139a53edb17f8e5931b6b6f9b314f144a9c633
MatheusGuilhermeRibeiro <matheusguilhermeribeirodev@gmail.com>
2024-10-21 05:23:20 -0300

    Feat: criacao do backend e api de registro do usuario

--- app/register-page/page.tsx
+++ app/register-page/page.tsx
@@ -65,7 +65,6 @@ export default function Page() {
             console.log('error')
         }
     }
-
     const handlerEmailChange = (newEmail: string) => {
         setuserEmail(newEmail);
     };
